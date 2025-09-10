7076a8d5885d23482b7a4256b0bbf6b90e880faa
adamhaosuser <adammmeehan@gmail.com>
2025-09-11 09:20:25 +1000

    Update index.js
    Add time return

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.post('/', (req, res) => {
   res.json({ createdStamp: timestamp });
 });
 
+app.get('/time', (req, res) => {
+  const nowUtc = new Date().toISOString();
+  res.json({ utc_time: nowUtc });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
